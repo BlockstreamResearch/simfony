3d65d4b341b628a4d46dfb280b29551be1ca9a92
Christian Lewe <ch_lewe@agdsn.me>
2024-04-03 21:48:33 +0900

    Pattern: Convert to base
    Array types and values are macros that don't exist in the underlying
    Simplicity. [1, 2, 3] is equivalent to ((1, 2), 3). In the next commit
    we will introduce array patterns of the form let [a, b, c] = [1, 2, 3].
    This is equivalent to let ((a, b), c) = ((1, 2), 3). We this translated
    pattern a _base pattern_ because it corresponds to the underlying
    Simplcity. As we add more types and patterns, it will be useful to rely
    on base patterns, to prevent too many edge cases.

--- src/parse.rs
+++ src/parse.rs
@@ -10,7 +10,7 @@ use simplicity::elements::hex::FromHex;
 use simplicity::types::Type as SimType;
 use simplicity::Value;
 
-use crate::array::{BTreeSlice, Partition};
+use crate::array::{BTreeSlice, BinaryTree, Partition};
 use crate::Rule;
 
 /// A complete simplicity program.
@@ -38,6 +38,39 @@ pub enum Pattern {
     Ignore,
     /// Split product value. Bind components to first and second pattern, respectively.
     Product(Arc<Self>, Arc<Self>),
+}
+
+impl Pattern {
+    /// Create an equivalent pattern that corresponds to the Simplicity base types.
+    ///
+    /// ## Base patterns
+    ///
+    /// - Identifier
+    /// - Ignore
+    /// - Product
+    pub fn to_base(&self) -> Self {
+        let binary = BinaryTree::from_tree(self);
+        let mut to_base = HashMap::new();
+
+        for data in binary.clone().post_order_iter() {
+            match data.node.as_node() {
+                Tree::Nullary => {
+                    let pattern = (*data.node.as_normal().unwrap()).clone();
+                    to_base.insert(data.node, pattern);
+                }
+                Tree::Binary(l, r) => {
+                    let l_converted = to_base.get(&l).unwrap().clone();
+                    let r_converted = to_base.get(&r).unwrap().clone();
+                    let pattern = Pattern::Product(Arc::new(l_converted), Arc::new(r_converted));
+                    to_base.insert(data.node, pattern);
+                }
+                Tree::Unary(..) => unreachable!("There are no unary patterns"),
+                Tree::Nary(..) => unreachable!("Binary trees have no arrays"),
+            }
+        }
+
+        to_base.remove(&binary).unwrap()
+    }
 }
 
 impl<'a> TreeLike for &'a Pattern {
