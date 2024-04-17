5f98c809c29f558767b891d5da991fa204d0736a
Christian Lewe <ch_lewe@agdsn.me>
2024-04-17 22:34:49 +0200

    Scope: Update documentation

--- src/scope.rs
+++ src/scope.rs
@@ -4,12 +4,53 @@ use crate::{named::ProgExt, ProgNode};
 
 /// Tracker of variable bindings.
 ///
-/// Internally there is a stack of scopes.
-/// A new scope is pushed for each (nested) block expression.
+/// Each Simfony expression expects an _input value_.
+/// A Simfony expression is translated into a Simplicity expression
+/// that similarly expects an _input value_.
+/// Simfony variable names are translated into Simplicity expressions
+/// that extract the seeked value from the input value of the encompassing expression.
 ///
-/// Bindings from higher scopes (in the stack) overwrite bindings from lower scopes.
+/// Each (nested) block expression introduces a new scope.
+/// Bindings from inner scopes overwrite bindings from outer scopes.
+/// Bindings live as long as their scope.
 #[derive(Debug, Clone)]
 pub struct GlobalScope {
+    /// For each scope, the set of assigned variables.
+    ///
+    /// A stack of scopes. Each scope is a stack of patterns.
+    /// New patterns are pushed onto the top _(current, innermost)_ scope.
+    ///
+    /// The stack of scopes corresponds to a _pattern tree_.
+    ///
+    /// The stack `[[p1], [p2, p3]]` corresponds to a nested product of patterns:
+    ///
+    /// ```text
+    ///    .
+    ///   / \
+    /// p3   .
+    ///     / \
+    ///   p2   p1
+    /// ```
+    ///
+    /// And so on.
+    ///
+    /// Inner scopes occur higher in the tree than outer scopes.
+    /// Later assignments occur higher in the tree than earlier assignments.
+    ///
+    /// The pattern tree is isomorphic to the _current input value_.
+    /// The isomorphism maps variable names from the pattern tree to values from the input.
+    ///
+    /// ```text
+    ///    .              .
+    ///   / \            / \
+    /// p3   .         v3   .
+    ///     / \            / \
+    ///   p2   p1   ↔    v2   v1
+    ///
+    /// p3 ↔ v3
+    /// p2 ↔ v2
+    /// p1 ↔ v1
+    /// ```
     variables: Vec<Vec<Pattern>>,
 }
 
@@ -27,40 +68,48 @@ impl GlobalScope {
         }
     }
 
-    /// Pushes a new scope to the stack.
+    /// Push a new scope onto the stack.
     pub fn push_scope(&mut self) {
         self.variables.push(Vec::new());
     }
 
-    /// Pops the latest scope from the stack.
+    /// Pop a scope from the stack.
     ///
     /// # Panics
     ///
-    /// Panics if the stack is empty.
+    /// The stack is empty.
     pub fn pop_scope(&mut self) {
-        self.variables.pop().expect("Popping scope zero");
+        self.variables.pop().expect("Empty stack");
     }
 
-    /// Pushes a new variable to the latest scope.
+    /// Push an assignment to the current scope.
+    ///
+    /// The input was updated in a let statement `let p = v`:
+    ///
+    /// ```text
+    ///   .
+    ///  / \
+    /// v   previous
+    /// ```
+    ///
+    /// Update the pattern tree accordingly:
+    ///
+    /// ```text
+    ///   .
+    ///  / \
+    /// p   previous
+    /// ```
     pub fn insert(&mut self, pattern: Pattern) {
         self.variables.last_mut().unwrap().push(pattern);
     }
 
-    /// Get a Simplicity expression that returns the value of the given `identifier`.
+    /// Get a Simplicity expression that returns the value of a variable.
     ///
-    /// The expression is a sequence of `take` and `drop` followed by `iden`,
-    /// which extracts the seeked value from the environment.
-    ///
-    /// The environment starts as the unit value.
-    ///
-    /// Each let statement updates the environment to become
-    /// the product of the assigned value (left) and of the previous environment (right).
-    ///
-    /// (Block) expressions consume the environment and return their output.
+    /// The expression takes the current input value and returns the seeked value.
     ///
     /// ## Example
     ///
-    /// ```
+    /// ```text
     /// let a: u8 = 0;
     /// let b = {
     ///     let b: u8 = 1;
@@ -69,27 +118,23 @@ impl GlobalScope {
     /// };
     /// ```
     ///
-    /// The stack of scopes looks like this:
-    ///
-    /// `[a] [b c]`
-    ///
-    /// The environment looks like this:
+    /// The pattern tree (left) and the input value (right) are as follows:
     ///
     /// ```text
-    ///   .
-    ///  / \
-    /// C   .
-    ///    / \
-    ///   B   .
-    ///      / \
-    ///     A   1
+    ///    .            .
+    ///   / \          / \
+    ///  c   .        2   .
+    ///     / \          / \
+    ///    b   .   ↔    1   .
+    ///       / \          / \
+    ///      a   _        0  ()
     /// ```
     ///
-    /// To extract `a`, we need the expression `drop drop take iden`.
+    /// The expression `drop drop take iden` returns the value `0` for variable `a`.
     ///
     /// ## Panics
     ///
-    /// The `identifier` is undefined.
+    /// The variable is not defined.
     pub fn get(&self, identifier: &Identifier) -> ProgNode {
         let mut pos = 0;
 
