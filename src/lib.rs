a36a42cdb9d59581cc55332aaf8741126e66bea5
Christian Lewe <ch_lewe@agdsn.me>
2024-04-18 15:07:51 +0200

    Example: Add list-fold

--- src/lib.rs
+++ src/lib.rs
@@ -161,6 +161,7 @@ mod tests {
         _test_progs("./example_progs/array.simpl");
         _test_progs("./example_progs/list.simpl");
         _test_progs("./example_progs/function.simf");
+        _test_progs("./example_progs/list_fold.simf");
     }
 
     fn _test_progs(file: &str) {
