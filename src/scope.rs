8477e265153d4864f228caa4c819e77f91e678bb
Christian Lewe <ch_lewe@agdsn.me>
2024-05-22 23:15:14 +0200

    Translate between two patterns
    Use PairBuilder to guarantee that the constructed Simplicity
    expressions contain only {pair, take, drop, iden}, meaning that
    pairs always unify.
    
    We might want to check uniqueness of identifiers inside patterns.

--- src/scope.rs
+++ src/scope.rs
@@ -225,6 +225,153 @@ impl BasePattern {
 
         true
     }
+
+    /// Get an iterator over all identifiers inside the pattern.
+    pub fn identifiers(&self) -> impl Iterator<Item = &Identifier> {
+        self.pre_order_iter().flat_map(BasePattern::as_identifier)
+    }
+
+    /// Check if all `identifiers` are contained inside the pattern.
+    pub fn contains_all<'a, I>(&self, mut identifiers: I) -> bool
+    where
+        I: Iterator<Item = &'a Identifier>,
+    {
+        identifiers.all(|id| self.contains(id))
+    }
+
+    /// Check if `self` covers the `other` pattern in terms of variable names.
+    ///
+    /// ## Coverage
+    ///
+    /// Pattern `p1` covers pattern `p2` if `p1` contains all variable names from `p2`.
+    pub fn covers(&self, other: &Self) -> bool {
+        self.contains_all(other.identifiers())
+    }
+
+    /// Check if the pattern is the ignore pattern.
+    pub fn is_ignore(&self) -> bool {
+        matches!(self, BasePattern::Ignore)
+    }
+
+    /// Check if the pattern contains an ignore pattern.
+    pub fn contains_ignore(&self) -> bool {
+        self.pre_order_iter().any(BasePattern::is_ignore)
+    }
+
+    /// Compute a Simplicity expression that takes as input a value that matches the `self` pattern
+    /// and that produces as output a value that matches the `to` pattern.
+    ///
+    /// ## Panics
+    ///
+    /// The `to` pattern contains ignore patterns: Every value matches the ignore pattern.
+    /// This means there are infinitely many translating expressions from `self` to `to`.
+    /// For instance, `iden`, `iden & iden`, `(iden & iden) & iden`, and so on.
+    /// We enforce a unique translation by banning ignore from the `to` pattern.
+    pub fn translate(&self, to: &Self) -> Option<ProgNode> {
+        #[derive(Debug, Clone)]
+        enum Task<'a> {
+            Translate(&'a BasePattern, &'a BasePattern),
+            MakeTake,
+            MakeDrop,
+            MakePair,
+        }
+
+        assert!(
+            !to.contains_ignore(),
+            "Ambiguous translation because `to` pattern contains ignore"
+        );
+        // Every variable in `to` needs a value which is extracted from `from`.
+        // If there are variables inside `to` that are not contained in `from`,
+        // then there is no translation from `from` to `to`.
+        if !self.covers(to) {
+            return None;
+        }
+
+        let mut stack = vec![Task::Translate(self, to)];
+        let mut output = vec![];
+
+        while let Some(task) = stack.pop() {
+            match task {
+                Task::Translate(from, to) => {
+                    debug_assert!(from.covers(to));
+
+                    match to {
+                        BasePattern::Ignore => {
+                            output.push(SelectorBuilder::default().h());
+                        }
+                        BasePattern::Identifier(to_id) => {
+                            output.push(from.get(to_id).map(SelectorBuilder::h)?);
+                        }
+                        BasePattern::Product(to_left, to_right) => {
+                            if to.subsumes(from) {
+                                // Every value that matches `from` also matches `to`.
+                                //
+                                // `iden` is the smallest expression that translates a value
+                                // that matches `from` into a value that matches `to`.
+                                //
+                                // The translated value is not always minimal with respect to
+                                // the pattern `to`. Here, we optimize for the size of the
+                                // translating expression and not for the size of the translated
+                                // value.
+                                output.push(SelectorBuilder::default().h());
+                            } else if let BasePattern::Product(from_left, from_right) = from {
+                                if from_right.covers(to) {
+                                    stack.push(Task::MakeDrop);
+                                    stack.push(Task::Translate(from_right, to));
+                                    continue;
+                                }
+                                if from_left.covers(to) {
+                                    stack.push(Task::MakeTake);
+                                    stack.push(Task::Translate(from_left, to));
+                                    continue;
+                                }
+
+                                stack.push(Task::MakePair);
+
+                                if from_right.covers(to_right) {
+                                    stack.push(Task::MakeDrop);
+                                    stack.push(Task::Translate(from_right, to_right));
+                                } else {
+                                    stack.push(Task::Translate(from, to_right));
+                                }
+                                if from_left.covers(to_left) {
+                                    stack.push(Task::MakeTake);
+                                    stack.push(Task::Translate(from_left, to_left));
+                                } else {
+                                    stack.push(Task::Translate(from, to_left));
+                                }
+                            } else {
+                                // Patterns contain no identifier duplicates.
+                                // The `to` pattern may not contain ignore patterns.
+                                // That is why, if the `to` pattern is a product,
+                                // then the `from` pattern must also be a product.
+                                // Otherwise, the `from` pattern would contain strictly fewer
+                                // variables than the `to` pattern, and there would be no
+                                // translation from `from` to `to`.
+                                unreachable!("The `from` pattern must be a product if the `to` pattern is a product");
+                            }
+                        }
+                    }
+                }
+                Task::MakeTake => {
+                    let translate = output.pop().unwrap();
+                    output.push(translate.take());
+                }
+                Task::MakeDrop => {
+                    let translate = output.pop().unwrap();
+                    output.push(translate.drop_());
+                }
+                Task::MakePair => {
+                    let translate_right = output.pop().unwrap();
+                    let translate_left = output.pop().unwrap();
+                    output.push(translate_left.pair(translate_right));
+                }
+            }
+        }
+
+        debug_assert_eq!(output.len(), 1);
+        output.pop().map(PairBuilder::get)
+    }
 }
 
 impl From<&Pattern> for BasePattern {
