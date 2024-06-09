e34d5126e87cabd52156b8db5562a6e4c70eba8a
Christian Lewe <ch_lewe@agdsn.me>
2024-06-09 13:45:30 +0200

    Add num::U256
    Representation of unsigned 256-bit values.
    
    I chose not to implement fmt::Display because it seemed like
    unnecessarily extra effort. We can implement fmt::Display in a follow-up
    PR. For now, fmt::Debug will display the hex representation.

--- src/num.rs
+++ src/num.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::str::FromStr;
+
 /// Implementation for newtypes that wrap a number `u8`, `u16`, ...
 /// such that the number has some property.
 /// The newtype needs to have a constructor `Self::new(inner) -> Option<Self>`.
@@ -104,5 +107,182 @@ impl DoublePow2Usize {
     /// The integer is equal to 2^(2^n). Return n.
     pub const fn log2_log2(self) -> u32 {
         self.0.trailing_zeros().trailing_zeros()
+    }
+}
+
+/// A 256-bit unsigned integer.
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct U256([u8; 32]);
+
+impl U256 {
+    /// The smallest value that can be represented by this integer type (0).
+    pub const MIN: Self = Self([0; 32]);
+    /// The largest value that can be represented by this integer type (2²⁵⁶ − 1).
+    pub const MAX: Self = Self([255; 32]);
+
+    /// Create a 256-bit unsigned integer from a byte array.
+    ///
+    /// The byte array is in Big Endian order.
+    pub fn from_byte_array(bytes: [u8; 32]) -> Self {
+        Self(bytes)
+    }
+
+    /// Convert the integer to a byte array.
+    ///
+    /// The byte array is in Big Endian order.
+    pub fn to_byte_array(self) -> [u8; 32] {
+        self.0
+    }
+}
+
+impl AsRef<[u8]> for U256 {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+
+impl From<u8> for U256 {
+    fn from(value: u8) -> Self {
+        let mut bytes = [0; 32];
+        bytes[31] = value;
+        Self(bytes)
+    }
+}
+
+impl From<u16> for U256 {
+    fn from(value: u16) -> Self {
+        let mut bytes = [0; 32];
+        let value_bytes = value.to_be_bytes();
+        bytes[30..].copy_from_slice(&value_bytes);
+        Self(bytes)
+    }
+}
+
+impl From<u32> for U256 {
+    fn from(value: u32) -> Self {
+        let mut bytes = [0; 32];
+        let value_bytes = value.to_be_bytes();
+        bytes[28..].copy_from_slice(&value_bytes);
+        Self(bytes)
+    }
+}
+
+impl From<u64> for U256 {
+    fn from(value: u64) -> Self {
+        let mut bytes = [0; 32];
+        let bot_eight = value.to_be_bytes();
+        bytes[24..].copy_from_slice(&bot_eight);
+        Self(bytes)
+    }
+}
+
+impl From<u128> for U256 {
+    fn from(value: u128) -> Self {
+        let mut bytes = [0; 32];
+        let value_bytes = value.to_be_bytes();
+        bytes[16..].copy_from_slice(&value_bytes);
+        Self(bytes)
+    }
+}
+
+impl FromStr for U256 {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let decimal = s.trim_start_matches('0');
+        // Short-circuit if string has more digits than u256::MAX
+        if 78 < decimal.chars().count() {
+            return Err(ParseIntError::PosOverflow);
+        }
+        let mut bytes = [0; 32];
+
+        for ch in decimal.chars() {
+            let mut carry = ch.to_digit(10).ok_or(ParseIntError::InvalidDigit)?;
+
+            // Add to the least significant bytes first
+            for byte in bytes.iter_mut().rev() {
+                let value = *byte as u32 * 10 + carry;
+                *byte = (value % 256) as u8;
+                carry = value / 256;
+            }
+            if 0 < carry {
+                return Err(ParseIntError::PosOverflow);
+            }
+        }
+
+        Ok(Self(bytes))
+    }
+}
+
+/// Reimplementation of [`std::num::ParseIntError`] that we can construct.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum ParseIntError {
+    InvalidDigit,
+    PosOverflow,
+}
+
+impl fmt::Display for ParseIntError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidDigit => write!(f, "Invalid decimal digit"),
+            Self::PosOverflow => write!(f, "Number too large to fit in target type"),
+        }
+    }
+}
+
+impl std::error::Error for ParseIntError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_u256_invalid_digit() {
+        assert_eq!(Err(ParseIntError::InvalidDigit), "a".parse::<U256>());
+    }
+
+    #[test]
+    fn parse_u256_overflow() {
+        let u256_max_plus_one =
+            "115792089237316195423570985008687907853269984665640564039457584007913129639936";
+        assert_eq!(
+            Err(ParseIntError::PosOverflow),
+            u256_max_plus_one.parse::<U256>()
+        );
+        let u256_max_times_ten =
+            "1157920892373161954235709850086879078532699846656405640394575840079131296399350";
+        assert_eq!(
+            Err(ParseIntError::PosOverflow),
+            u256_max_times_ten.parse::<U256>()
+        );
+    }
+
+    #[test]
+    fn parse_u256_leading_zeroes() {
+        assert_eq!(U256::MIN, "00".parse().unwrap());
+        assert_eq!(
+            U256::MAX,
+            "0115792089237316195423570985008687907853269984665640564039457584007913129639935"
+                .parse()
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_u256_ok() {
+        for n in 0u8..=255 {
+            let s = n.to_string();
+            assert_eq!(U256::from(n), s.parse().unwrap());
+        }
+        assert_eq!(
+            U256::from(u128::MAX),
+            "340282366920938463463374607431768211455".parse().unwrap(),
+        );
+        assert_eq!(
+            U256::MAX,
+            "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+                .parse()
+                .unwrap()
+        );
     }
 }
