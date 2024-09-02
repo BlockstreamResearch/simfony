b8dcb01a147db85b913e2eb0ef5b7f1f991ea5e6
Christian Lewe <ch_lewe@agdsn.me>
2024-09-02 16:31:31 +0200

    fix: Make bitcoind-tests compile
    Exclude bitcoind-tests from main workspace. Update dependencies.
    Update list of tested programs. Most programs cannot yet be tested
    because they require signatures that need to be dynamically generated
    during the test.

--- bitcoind-tests/tests/test_arith.rs
+++ bitcoind-tests/tests/test_arith.rs
@@ -15,7 +15,10 @@ use elements::{
     confidential, pset as psbt, secp256k1_zkp as secp256k1, OutPoint, Script, Sequence,
     TxIn, TxOut, Txid,
 };
+use elementsd::bitcoincore_rpc::jsonrpc::serde_json;
 use elementsd::ElementsD;
+use simfony::witness::WitnessValues;
+
 mod setup;
 use setup::Call;
 
@@ -34,15 +37,19 @@ fn get_vout(cl: &ElementsD, txid: Txid, value: u64, spk: Script) -> (OutPoint, T
     unreachable!("Only call get vout on functions which have the expected outpoint");
 }
 
-pub fn test_simplicity(cl: &ElementsD, prog: &str, witness_file: &str) {
-    let prog = Path::new(prog);
-    let witness_file = Path::new(witness_file);
+pub fn test_simplicity(cl: &ElementsD, program_file: &str, witness_file: &str) {
+    let program_path = Path::new(program_file);
+    let witness_path = Path::new(witness_file);
+    let program_text = std::fs::read_to_string(program_path).unwrap();
+    let witness_text = std::fs::read_to_string(witness_path).unwrap();
+    let witness_values = serde_json::from_str::<WitnessValues>(&witness_text).unwrap();
+    let program = simfony::satisfy(&program_text, &witness_values).unwrap();
+
     let secp = secp256k1::Secp256k1::new();
     let internal_key = XOnlyPublicKey::from_str("f5919fa64ce45f8306849072b26c1bfdd2937e6b81774796ff372bd1eb5362d2").unwrap();
 
-    let commit_prog = simfony::compile(&prog);
     let builder = TaprootBuilder::new();
-    let script = elements::script::Script::from(commit_prog.cmr().as_ref().to_vec());
+    let script = elements::script::Script::from(program.cmr().as_ref().to_vec());
     let script_ver = (script, LeafVersion::from_u8(0xbe).unwrap());
     let builder = builder.add_leaf_with_ver(0, script_ver.0.clone(), script_ver.1).unwrap();
     let data = builder.finalize(&secp, internal_key).unwrap();
@@ -72,10 +79,11 @@ pub fn test_simplicity(cl: &ElementsD, prog: &str, witness_file: &str) {
     psbt.add_output(psbt::Output::from_txout(out));
     let fee_out = TxOut::new_fee(3_000, witness_utxo.asset.explicit().unwrap());
     psbt.add_output(psbt::Output::from_txout(fee_out));
-    let redeem_prog = simfony::satisfy(prog, witness_file);
+    let (program_bytes, witness_bytes) = program.encode_to_vec();
     psbt.inputs_mut()[0].final_script_witness =
     Some(vec![
-        redeem_prog.encode_to_vec(),
+        witness_bytes,
+        program_bytes,
         script_ver.0.clone().into_bytes(),
         data.control_block(&script_ver).unwrap().serialize(),
     ]);
@@ -88,14 +96,8 @@ pub fn test_simplicity(cl: &ElementsD, prog: &str, witness_file: &str) {
 #[test]
 fn test_arith() {
     let (cl, _genesis_hash) = &setup::setup();
-    // let testdata = TestData::new_fixed_data(50, *genesis_hash);
     println!("{}", cl.get_new_address());
 
-    test_simplicity(cl, "../example_progs/add.simpl", "../example_progs/empty.wit");
-    test_simplicity(cl, "../example_progs/scopes.simpl", "../example_progs/empty.wit");
-    // test_simplicity(cl, "../example_progs/add_with_builtins.simpl", "../example_progs/empty.wit");
-    // test_simplicity(cl, "../example_progs/assertr.simpl", "../example_progs/empty.wit");
-    test_simplicity(cl, "../example_progs/nesting.simpl", "../example_progs/empty.wit");
-    test_simplicity(cl, "../example_progs/test.simpl", "../example_progs/test.wit");
-    // test_descs(cl, &testdata);
+    test_simplicity(cl, "../examples/cat.simf", "../examples/empty.wit");
+    // TODO: Other examples require custom signatures
 }
