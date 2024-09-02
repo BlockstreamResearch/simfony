f9a1a7d18918c4cf8e4b48feef46a7b13c7505af
Christian Lewe <ch_lewe@agdsn.me>
2024-09-02 16:31:41 +0200

    chore: Update elementsd nix shell

--- bitcoind-tests/shell.nix
+++ bitcoind-tests/shell.nix
@@ -1,12 +1,12 @@
 with (import <nixpkgs> {});
 let
   elementsd-simplicity = elementsd.overrideAttrs (_: rec {
-    version = "unstable-2023-11-22";
+    version = "unstable-2024-09-02";
     src = fetchFromGitHub {
       owner = "ElementsProject";
       repo = "elements";
-      rev = "174c46baecd7b180cda977827886833840ac49e3"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
-      sha256 = "sha256-kSN3Rj7YdFoBibOFdKq7f6RTaVamN1dFjuL3wPOu5Kw="; # <-- overwrite this, rerun and place the expected hash
+      rev = "37c231153a9c61f02f932c2cb802656ae47cff32"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
+      sha256 = "sha256-QYywe7lfBQIjp7CecxA/6/XsWAOaBckGbT432Xe2ru0="; # <-- overwrite this, rerun and place the expected hash
     };
     withWallet = true;
     withGui = false;
