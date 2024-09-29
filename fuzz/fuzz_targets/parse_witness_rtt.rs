70535b6cbc7456a988e80f32bc478ecf82bc8844
Christian Lewe <ch_lewe@agdsn.me>
2024-09-29 16:47:57 +0200

    test: Parse witness maps
    Convert a witness map to a JSON and parse the JSON back into the
    original map.

---
+++ fuzz/fuzz_targets/parse_witness_rtt.rs
@@ -0,0 +1,16 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use simfony::witness::WitnessValues;
+
+fuzz_target!(|witness_values: WitnessValues| {
+    let witness_text =
+        serde_json::to_string(&witness_values).expect("Witness map should be convertible into JSON");
+    let parsed_witness_text =
+        serde_json::from_str(&witness_text).expect("Witness JSON should be parseable");
+    assert_eq!(
+        witness_values, parsed_witness_text,
+        "Witness JSON should parse to original witness map"
+    );
+});
