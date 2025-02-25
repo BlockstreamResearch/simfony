c0c62db3749c206a37af5213f16896f948c59acc
Christian Lewe <ch_lewe@agdsn.me>
2025-02-25 16:41:05 +0100

    chore: Mark methods as const

--- src/value.rs
+++ src/value.rs
@@ -607,12 +607,12 @@ impl From<UIntValue> for Value {
 
 impl Value {
     /// Access the inner structure of the value.
-    pub fn inner(&self) -> &ValueInner {
+    pub const fn inner(&self) -> &ValueInner {
         &self.inner
     }
 
     /// Access the type of the value.
-    pub fn ty(&self) -> &ResolvedType {
+    pub const fn ty(&self) -> &ResolvedType {
         &self.ty
     }
 
